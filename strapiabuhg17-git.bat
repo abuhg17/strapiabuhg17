4517cd0cb55dd9c97f937853157b4c3e9e473b42
goldshoot0720 <goldshoot0720@gmail.com>
2025-11-06 02:54:27 +0800

    20251106v1

---
+++ strapiabuhg17-git.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "20251106v1"
+git remote add origin https://github.com/abuhg17/strapiabuhg17.git
+git checkout -b main
+git push -u origin main --force
+pause
---
+++ strapiabuhg17-open.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\chbon\Downloads\strapiabuhg17"
+explorer http://localhost:1337/admin
+npm run develop
+pause
